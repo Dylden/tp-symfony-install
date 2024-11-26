e40d521a440a79635619fe5a0c97406c411d0821
Dylden <dylan.dennig@lapiscine.pro>
2024-11-26 10:43:19 +0100

    article_show

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -50,4 +51,69 @@ class ArticleController extends AbstractController{
             'articles' => $articles
         ]);
     }
+
+    #[Route('/article','article_show')]
+    public function showArticle(){
+
+        //Tableau d'articles
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Content of article 1',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Content of article 2',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Content of article 3',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 4,
+                'title' => 'Article 4',
+                'content' => 'Content of article 4',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 5,
+                'title' => 'Article 5',
+                'content' => 'Content of article 5',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ]
+
+        ];
+
+        //Je créé une requête avec une une méthode statique "createFromGlobals"
+        $request = Request::createFromGlobals();
+
+        //Je créé une variable "id" pour récupérer ses données avec GET
+        $id = $request->query->get('id');
+
+
+        //Je créé une variable me permettant de stocker la valeur de l'id de l'article
+        //null par défaut
+        $articleFound = null;
+
+
+        //Permet de récupérer la valeur stockée dans articleFound
+        foreach ($articles as $article) {
+            if($article['id'] == (int) $id){
+                $articleFound = $article;
+            }
+
+
+        }
+
+        return $this->render('article_show.html.twig', [
+            'article' => $articleFound
+        ]);
+    }
+
 }
