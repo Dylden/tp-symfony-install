e64caf917ec903e223a8deb7dd15cf8652db8eb1
Dylden <dylan.dennig@lapiscine.pro>
2024-11-27 11:03:39 +0100

    add contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,9 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+
+//Ajout de la classe ContactController
 class ContactController extends AbstractController
 {
+    //La route pour accéder à cette page est /contact
     #[Route('/contact', name: 'contact')]
+
+    //Je fais une fonction contact
+    //qui me sert à récupérer les données POST de mon formulaire
     public function contact(Request $request)
     {
         $name = $request->request->get('name');
